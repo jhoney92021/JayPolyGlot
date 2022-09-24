935bcc8f791469ee077d7055fe568d89a433bb77
justin honey <jhoney92021@gmail.com>
2022-09-24 17:34:37 -0600

    chore: clean up deleted methods

--- Ruby/Program.rb
+++ Ruby/Program.rb
@@ -59,27 +59,3 @@ ShiftArray.DoUntil(TestIntegerArray)
 # FizzBuzz.For(TestFizzBuzzArray)
 # FizzBuzz.While(TestFizzBuzzArray)
 # FizzBuzz.DoUntil(TestFizzBuzzArray)
-
-# FindOddValues.For(TestIntegerArray)
-# FindOddValues.While(TestIntegerArray)
-# FindOddValues.DoUntil(TestIntegerArray)
-
-# For_Print_To255.Do()
-# While_Print_To255.Do()
-# While_Print_To255Odds.Do()
-# For_Print_Sum.Do()
-# Until_Print_Sum.Do()
-# While_Print_Sum.Do()
-# testMax = 34
-# testArray = [0,9,6,1,testMax]
-# realMax = For_Find_Max.Do(testArray)
-# For_Get_Average.Do(testArray)
-# Odd_Array.Parse(testArray)
-# GreaterThanY.Parse(testArray,7)
-
-# toSquare = [1,5,10,-10]
-# Square_Array.SquareEm(toSquare)
-
-# superNegative = [-1,-5,-25,-10]
-# Positive_Array.Robbins(superNegative)
-
