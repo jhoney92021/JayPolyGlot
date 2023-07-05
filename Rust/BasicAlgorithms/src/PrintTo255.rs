c4235ab881e50cf25c655bc35b24f43d961e4cf8
justin honey <jhoney92021@gmail.com>
2023-07-05 06:51:38 -0600

    feat: use while loop

--- Rust/BasicAlgorithms/src/PrintTo255.rs
+++ Rust/BasicAlgorithms/src/PrintTo255.rs
@@ -1,5 +1,12 @@
-pub fn PrintTo255() {
+pub fn PrintTo255_ForIn() {
     for i in 1..256 {
         println!("{}", i);
+    }
+}
+pub fn PrintTo255_While() {
+    let mut i = 0;
+    while i < 256 {
+        println!("{}", i);
+        i += 1;
     }
 }
