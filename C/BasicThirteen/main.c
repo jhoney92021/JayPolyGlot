954ea08d6e8b98c9e5ae82850114aa66ae296451
jhoney <jhoney92021@gmail.com>
2023-12-18 15:47:54 -0700

    doc: more docs start cpp
    prettier

--- C/BasicThirteen/main.c
+++ C/BasicThirteen/main.c
@@ -9,74 +9,92 @@
 #define WHITE_TEXT "\033[1;37m"
 #define RESET_TEXT "\033[0m"
 // Function to print a new line
-void printNewLine() {
+void printNewLine()
+{
     printf("\n");
 }
 // Function to print text with color and formatting
-void printIntWithColor(const char *text, const int *value, const char *color) {
+void printIntWithColor(const char *text, const int *value, const char *color)
+{
     // If color is NULL, default to GREEN_TEXT
-    if (color == NULL) {
+    if (color == NULL)
+    {
         color = GREEN_TEXT;
     }
     printf("%s~~~ %s %d ~~~%s\n", color, text, value, RESET_TEXT);
 }
 // Function to print text with color and formatting
-void printWithColor(const char *text, const char *color) {
+void printWithColor(const char *text, const char *color)
+{
     // If color is NULL, default to GREEN_TEXT
-    if (color == NULL) {
+    if (color == NULL)
+    {
         color = GREEN_TEXT;
     }
     printf("%s~~~ %s ~~~%s\n", color, text, RESET_TEXT);
 }
 // Function to print the array
-void printArray(int size, int intArr[]) {
+void printArray(int size, int intArr[])
+{
     printWithColor("Print Array", GREEN_TEXT);
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         printf("%d", intArr[i]);
         printNewLine();
     }
 }
 // Function to print the maximum value in the array
-void printMaxArray(int size, int intArr[]) {
+void printMaxArray(int size, int intArr[])
+{
     printWithColor("Print Max Array", GREEN_TEXT);
     int max = intArr[0];
-    for (int i = 0; i < size; i++) {
-        if (max < intArr[i]) {
+    for (int i = 0; i < size; i++)
+    {
+        if (max < intArr[i])
+        {
             max = intArr[i];
         }
     }
     printf("~~~ max ~~~\n%d\n", max);
 }
 // Function to print the average value of the array
-void printAvgArray(int size, int intArr[]) {
+void printAvgArray(int size, int intArr[])
+{
     int sum = intArr[0];
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i < size; i++)
+    {
         sum += intArr[i];
     }
     int average = sum / size;
     // Use RED_TEXT for the "average" message
     printIntWithColor("Print Avg Array", average, RED_TEXT);
 }
-int* findOddValues_For(int integerArray[], int arrayLength, int* resultSize) {
-    int* oddArray = NULL;
+int *findOddValues_For(int integerArray[], int arrayLength, int *resultSize)
+{
+    int *oddArray = NULL;
     int oddCount = 0;
     // Count the number of odd values in the input array
-    for (int iterator = 0; iterator < arrayLength; iterator++) {
-        if (integerArray[iterator] % 2 == 1) {
+    for (int iterator = 0; iterator < arrayLength; iterator++)
+    {
+        if (integerArray[iterator] % 2 == 1)
+        {
             oddCount++;
         }
     }
     // Allocate memory for the result array
-    oddArray = (int*)malloc(oddCount * sizeof(int));
-    if (oddArray == NULL) {
+    oddArray = (int *)malloc(oddCount * sizeof(int));
+    if (oddArray == NULL)
+    {
         // Handle memory allocation failure
         *resultSize = 0;
         return NULL;
     }
     // Fill the result array with odd values
     int resultIndex = 0;
-    for (int iterator = 0; iterator < arrayLength; iterator++) {
-        if (integerArray[iterator] % 2 == 1) {
+    for (int iterator = 0; iterator < arrayLength; iterator++)
+    {
+        if (integerArray[iterator] % 2 == 1)
+        {
             oddArray[resultIndex++] = integerArray[iterator];
         }
     }
@@ -84,23 +102,25 @@ int* findOddValues_For(int integerArray[], int arrayLength, int* resultSize) {
     *resultSize = oddCount;
     // Print the result array for validation
     printf("~~~ To Odd Array -- For Loop -- Values: ");
-    for (int i = 0; i < oddCount; i++) {
+    for (int i = 0; i < oddCount; i++)
+    {
         printf("%d ", oddArray[i]);
     }
     printf("~~~\n");
     return oddArray;
 }
-int main() {
+int main()
+{
     // printWithColor("1 - 255", MAGENTA_TEXT);
     int numArr[4] = {1, 4453, 67, 5};
     // printArray(4, numArr);
     // printMaxArray(4, numArr);
     printAvgArray(4, numArr);
     printWithColor("Find Max Value", CYAN_TEXT);
-        int inputArray[] = {1, 12, 3, 4, 6};
+    int inputArray[] = {1, 12, 3, 4, 6};
     int arrayLength = sizeof(inputArray) / sizeof(inputArray[0]);
     // Variables to store the result array and its size
-    int* resultArray;
+    int *resultArray;
     int resultSize;
     // Call the function to find odd values
     resultArray = findOddValues_For(inputArray, arrayLength, &resultSize);
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JayPolyGlot -- learning programming basic cross lang #
+# JayPolyGlot -- Learning Programming Fundamentals Across Languages #
 
 ### Quick Links ###
 * [Git](./Git/README.md)
@@ -18,11 +18,9 @@
 * [VB .NET Basics](./VisualBasic/README.md)
 
 ## Intent ##
-* The basic intent of this repo is to practice
-    basic programming across language IE C#, Java, Ruby, etc
-* Most things written should have some pairity in that
-    it should be the same programs pretty much
-    just with a different language/framework etc.
+* The primary goal of this repository is to facilitate practice in basic programming across various languages, including C#, Java, Ruby, etc.
+* The content within each language's folder should exhibit a level of parity, allowing users to compare similar programs implemented in different languages/frameworks.
 
 ## Docs ##
-* Each sub folder should have it's own README on how to open/run install blah blah blah
+* Each subfolder contains a dedicated README providing instructions on how to open, run, and install the respective codebase.
+* Detailed documentation ensures a smooth onboarding experience for users exploring different languages and concepts within the repository.
